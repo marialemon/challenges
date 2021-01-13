eb1ed89672f6bda8f3862514938b8f8de555da49
marialemon <62708210+marialemon@users.noreply.github.com>
2021-01-13 15:40:45 +0100

    mobile first wip

---
+++ article-preview-component-master/main.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener("click", function(){
+    document.getElementById("share").innerHTML;
+    
+    document.getElementById("active_share").style.visibility = "visible";
+    
+});
+
